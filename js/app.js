5a632617336fc5de2c1e87dcd074d5f4d4d31f37
cutiegirl526 <kayladiniega@gmail.com>
2016-12-12 08:20:06 -1000

    frustrations

---
+++ js/app.js
@@ -0,0 +1,12 @@
+console.log("HELP!");
+
+function hello (){
+	console.log("hello");
+	var changeDiv = document.getElementById('divElement');
+	changeDiv.innerHTML = "what";
+	console.log(changeDiv);
+}
+
+function sofast() {
+	var classElements = document.getElementsByClassName()
+}
